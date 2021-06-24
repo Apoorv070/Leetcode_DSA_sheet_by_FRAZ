016089d60e7b27467d1068a551cc6bc01bdafc10
Apoorv <47479228+Apoorv070@users.noreply.github.com>
2021-06-24 13:15:34 +0530

    Create Binary Tree Right Side View.cpp

---
+++ Trees/Medium/Binary Tree Right Side View.cpp
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/binary-tree-right-side-view/
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+    vector<vector<int>> v;
+     queue<TreeNode*> q;
+     if(root)
+     q.push(root);
+     if(root)
+     q.push(NULL);
+     vector<int> v1;
+     while(q.size()!=0){
+         if(q.front()==NULL){
+             
+             q.pop();
+             if(q.size()!=0){
+                 q.push(NULL);
+             }
+             v.push_back(v1);
+             v1.clear();
+         }else{
+             TreeNode* n = q.front();
+             if(n->left)
+             q.push(n->left);
+             if(n->right)
+             q.push(n->right);
+             q.pop();
+             v1.push_back(n->val);
+         }
+         
+     }
+        vector<int> v2;
+    for(int i = 0 ; i < v.size() ; i++){
+      v2.push_back(v[i].back());  
+    }
+        
+     return v2;   
+        
+    }
+};
