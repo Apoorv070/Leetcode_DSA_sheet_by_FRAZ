e1c30b3f64b08178702397d113136c39492fd4f7
Apoorv <47479228+Apoorv070@users.noreply.github.com>
2021-06-24 09:23:41 +0530

    Create Pathsum.cpp

---
+++ Trees/Easy/Pathsum.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/path-sum/
+class Solution {
+public:
+	bool hasPathSum(TreeNode* root, int targetSum) {
+		if(root == NULL){
+			return false;
+		}
+		if(root->left == root->right){
+			return targetSum == root->val;
+		}
+		return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+	}
+};
