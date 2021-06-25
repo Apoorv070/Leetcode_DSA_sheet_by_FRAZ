0c0ac09984461a54f41599b54ebf0794c9210f1b
Apoorv <47479228+Apoorv070@users.noreply.github.com>
2021-06-25 15:43:33 +0530

    Update 3SumClosest.cpp

--- Arrays/Medium/3SumClosest.cpp
+++ Arrays/Medium/3SumClosest.cpp
@@ -5,7 +5,6 @@ public:
     int closest = nums[0]+nums[1]+nums[2];
     sort(nums.begin(), nums.end());
     for(int first = 0 ; first < nums.size()-2 ; ++first) {
-        if(first > 0 && nums[first] == nums[first-1]) continue;
         int second = first+1;
         int third = nums.size()-1;            
         while(second < third) {
