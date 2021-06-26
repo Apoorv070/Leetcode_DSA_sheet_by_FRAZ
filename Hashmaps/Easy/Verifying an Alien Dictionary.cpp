c8359ae354bdaccc25e15a0c02bc4b71211e53cd
Apoorv <47479228+Apoorv070@users.noreply.github.com>
2021-06-26 08:09:40 +0530

    Update Verifying an Alien Dictionary.cpp

--- Hashmaps/Easy/Verifying an Alien Dictionary.cpp
+++ Hashmaps/Easy/Verifying an Alien Dictionary.cpp
@@ -8,15 +8,17 @@ public:
         for (int i = 0; i < order.size(); i++) {
             map[order[i]] = i;
         } 
+        bool ans = true;
         for (int i = 0; i < words.size() - 1; i++) {
             string word1 = words[i], word2 = words[i+1];
             int j;
+            if(word1==word2)continue;
             for (j = 0; j < word1.size() and j < word2.size(); j++) { // compare character by character.
                 if (map[word1[j]] > map[word2[j]]) return false; // wrong order.
                 else if (map[word1[j]] < map[word2[j]]) break; // correct order.
             }
             if (j == word2.size()) return false; // word1 is longer than word2
         }
-        return true;
+        return ans;
     }
 };
