895e5ed89f17c68f8f4b1475d54c1d17fe725e3b
Apoorv <47479228+Apoorv070@users.noreply.github.com>
2021-06-28 18:26:12 +0530

    Create cycle detection.cpp

---
+++ LinkedLists/Easy/cycle detection.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+    // if(head==NULL)
+    //     return false;
+     ListNode * first = head;   
+     ListNode * second = head;   
+    while(second!=NULL&&second->next!=NULL){
+        first = first->next;
+        second = second->next->next;
+        if(first==second)
+            return true;
+    }
+    return false;
+        
+    }
+};
