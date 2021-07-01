90fcb7075776b92b5a3d9158a87136a289fe4097
Apoorv <47479228+Apoorv070@users.noreply.github.com>
2021-07-01 12:14:43 +0530

    Update Continuous Subarray Sum.cpp

--- DynamicProgramming/Medium/Continuous Subarray Sum.cpp
+++ DynamicProgramming/Medium/Continuous Subarray Sum.cpp
@@ -3,10 +3,12 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         map<int,int> m;
         int sum = 0;
+        int prev = 0;
         for(int i = 0 ; i < nums.size() ; i++){
-            
-            if(m.find(sum)!=m.end() and abs(i-m[sum])>1)return true;
-            m[sum] = i;
+            sum = (k==0) ? (sum + nums[i]) : (sum + nums[i]) % k;
+            if(m.find(sum)!=m.end())return true;
+            m[prev]++;
+            prev = sum;
         }
         return false;
     }
