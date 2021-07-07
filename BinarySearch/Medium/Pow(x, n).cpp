186aa0bcde6fd6432a09904edea7782be98072ad
Apoorv <47479228+Apoorv070@users.noreply.github.com>
2021-07-07 17:13:04 +0530

    Create Pow(x, n).cpp

---
+++ BinarySearch/Medium/Pow(x, n).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+      
+        double result = 1;
+        while(n){
+            if(abs(n%2)==1){
+                result = (n>0)?result*x: result/x;
+            }
+            x=x*x;
+            n=n/2;
+            
+        }
+        return result;
+    }
+};
